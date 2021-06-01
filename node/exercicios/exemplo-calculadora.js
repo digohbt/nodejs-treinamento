8a7223a719626166a46f849c508e75b556774fc2
Diego Teodoro <digohbt@gmail.com>
2021-06-01 12:17:28 -0300

    ref função calculadora

--- node/exercicios/exemplo-calculadora.js
+++ node/exercicios/exemplo-calculadora.js
@@ -1,13 +1,23 @@
 const calculadora = require ("./calculadora")
+console.log("======= Soma ==========")
+console.log(calculadora.somar(2, 5))
+console.log(calculadora.somar2(2,1,1,1,1))
+console.log("======= Fim ==========")
 
-// console.log(calculadora.somar(2, 5))
-// console.log(calculadora.somar2(2,1,1,1,1))
+console.log("======= Subtração ==========")
 
-// console.log(calculadora.sub(5, 2))
-// console.log(calculadora.sub2(5,1,1,1))
+console.log(calculadora.sub(5, 2))
+console.log(calculadora.sub2(5,1,1,1))
+console.log("======= Fim ==========")
+
+console.log("======= Divisão ==========")
 
 console.log(calculadora.div(5, 1))
 console.log(calculadora.div2(10,2,1,1))
+console.log("======= Fim ==========")
 
-// console.log(calculadora.mult(5, 2))
-// console.log(calculadora.mult2(10,2,2,2))
+console.log("======= multiplicação ==========")
+
+console.log(calculadora.mult(5, 2))
+console.log(calculadora.mult2(10,2,2,2))
+console.log("======= Fim ==========")
