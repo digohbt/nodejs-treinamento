6b9d6752681a92ab970c31a07aa9878450df73c4
Diego Teodoro <digohbt@gmail.com>
2021-06-02 16:21:51 -0300

    Exercicio server

---
+++ node/exercicios/02-exercicio/app.js
@@ -0,0 +1,42 @@
+const http = require("http");
+const fs = require('fs') 
+
+
+const server = http.createServer( 
+    (request , response ) => {
+        
+        let caminho_arquino = ""
+        if(request.url === "/"){
+                caminho_arquino = "index.html";
+        }else {
+             caminho_arquino = `${request.url.replace("/","")}.html`
+            // console.log("===",segunda.replace("/",""))
+        }
+        // }else if (request.url === "/carrinho"){
+        //         caminho_arquino = "carrinho.html";
+
+        // }else if (request.url === "/produtos"){
+        //         caminho_arquino = "produtos.html";
+        // }
+    
+       
+       if(fs.existsSync(caminho_arquino)){
+            fs.readFile(caminho_arquino , 
+                (erro , dados)=> {
+                    if(erro){
+                        console.log("===erro======");
+                    }
+                    response.writeHead(200, {'Content-Type': 'Text/html'});
+                    response.end(dados)
+        })
+       }else {
+        response.writeHead(404, {'Content-Type': 'Text/html'});
+        response.write("Pagina nao encontrada");
+        response.end();
+    }
+    
+});
+
+
+
+server.listen(3000, () => console.log("Servidor porta 3000"))
